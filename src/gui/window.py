82ed369dc1a6d04920fd5f2d29ea78726a3f2204
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-11-18 20:53:22 -0500

    clean up widget generation
    Co-authored-by: Selisine <Aidenpike113@gmail.com>

--- src/gui/window.py
+++ src/gui/window.py
@@ -4,24 +4,25 @@ import utils
 def create_widgets(screen: pygame.Surface) -> None:
     canvas = pygame.Surface(utils.WINDOW_DIMENSIONS)
 
+    width = utils.WINDOW_DIMENSIONS[0]
+    height = utils.WINDOW_DIMENSIONS[1]
+
     blocks_panel = pygame.Rect(
-        0, 0,
-        utils.WINDOW_DIMENSIONS[0] // 5,
-        utils.WINDOW_DIMENSIONS[1]
+        0, 0, width // 5, height
     )
 
-    preview_window_dimensions = (utils.WINDOW_DIMENSIONS[0] // 4, utils.WINDOW_DIMENSIONS[0] // 6)
+    preview_window_dimensions = (
+        width // 4, width // 6
+    )
     preview_window = pygame.Rect(
-        0, 0,
-        preview_window_dimensions[0],
-        utils.WINDOW_DIMENSIONS[1]
+        0, 0, preview_window_dimensions[0], height
     )
 
     screen.blit(canvas, (0, 0), blocks_panel)
     screen.blit(
         canvas, (
-            utils.WINDOW_DIMENSIONS[0] - preview_window_dimensions[0],
-            -(utils.WINDOW_DIMENSIONS[1] - preview_window_dimensions[1])
+            width - preview_window_dimensions[0],
+            -(height - preview_window_dimensions[1])
         ),
         preview_window
     )
